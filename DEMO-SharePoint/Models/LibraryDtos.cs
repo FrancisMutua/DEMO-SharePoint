acb7e94ade4beae120cad40711aefa956fef37ef
FrancisMutua <francismutua664@gmail.com>
2026-02-26 18:05:58 +0300

    initial changes

---
+++ DEMO-SharePoint/Models/LibraryDtos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KCAU_SharePoint.Models
+{
+    public class LibraryDto
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Icon { get; set; }
+        public string Access { get; set; }
+        public string Desc { get; set; }
+        public List<MetaFieldDto> Fields { get; set; }
+    }
+
+    public class MetaFieldDto
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public bool Required { get; set; }
+    }
+}
